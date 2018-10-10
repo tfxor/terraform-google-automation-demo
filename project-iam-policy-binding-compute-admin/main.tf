fda43eeba41dd0f1f9e1266168492cd7ec048eb5
root <root@DESKTOP-ECSQPB1.localdomain>
2018-10-10 16:58:14 +0200

    remove null line

--- project-iam-policy-binding-compute-admin/main.tf
+++ project-iam-policy-binding-compute-admin/main.tf
@@ -1,6 +1,5 @@
 resource "google_project_iam_binding" "project-iam-policy-binding-compute-admin" {
   project = "${var.google_project_id}"
   role    = "${var.google_project_role}"
-
   members = "${var.google_project_members}"
 }
