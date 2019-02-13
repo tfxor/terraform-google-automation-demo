e37da5350f274cf864e5b51e6eeebb16a1c14ef3
root <root@DESKTOP-ECSQPB1.localdomain>
2019-02-13 13:17:08 +0100

    convert HCL to THub config

--- project_iam_member/main.tf
+++
@@ -1,5 +0,0 @@
-resource "google_project_iam_member" "project_iam_member" {
-  project = "${var.google_project_id}"
-  role    = "${var.google_project_role}"
-  member  = "${var.google_project_member}"
-}
