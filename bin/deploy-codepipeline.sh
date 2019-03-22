d394d7e8c12ab36636e291a8e61849bc14a7e600
Vitalie Calmîc <vcalmic@mitocgroup.com>
2019-03-23 00:50:18 +0200

    Update script.

--- bin/deploy-codepipeline.sh
+++ bin/deploy-codepipeline.sh
@@ -15,9 +15,9 @@ git checkout $BRANCH_FROM
 
 jq --version > /dev/null 2>&1 || { echo >&2 'jq is missing. aborting...'; exit 1; }
 GOOGLE_CLOUD_PROJECT="$(gcloud config list --format=json | jq '.core.project')"
-cat $GOOGLE_APPLICATION_CREDENTIALS_CONTENT > ${HOME}/.config/gcloud/${GOOGLE_CLOUD_PROJECT}.json
+cat $GOOGLE_APPLICATION_CREDENTIALS_CONTENT > ${HOME}/${GOOGLE_CLOUD_PROJECT}.json
 BILLING_ID="$(gcloud beta billing accounts list --format=json | jq '.[0].name[16:]')"
-GOOGLE_APPLICATION_CREDENTIALS="${HOME}/.config/gcloud/${GOOGLE_CLOUD_PROJECT}.json"
+GOOGLE_APPLICATION_CREDENTIALS="${HOME}/${GOOGLE_CLOUD_PROJECT}.json"
 SERVICE_ACCOUNT="terraform@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com"
 
 terrahub --version > /dev/null 2>&1 || { echo >&2 "terrahub is missing. aborting..."; exit 1; }
