1da50d741442753d6edc149d6266d4705d1936d6
Eugene <eistrati@mitocgroup.com>
2019-03-23 13:36:56 -0400

    more debugging

--- bin/deploy-codepipeline.sh
+++ bin/deploy-codepipeline.sh
@@ -14,13 +14,13 @@ git --version > /dev/null 2>&1 || { echo >&2 'git is missing. aborting...'; exit
 git checkout $BRANCH_TO
 git checkout $BRANCH_FROM
 
-GOOGLE_CLOUD_PROJECT="$(gcloud config list --format=json | jq '.core.project')"
+GOOGLE_CLOUD_PROJECT=$(gcloud config list --format=json | jq '.core.project')
 echo "GOOGLE_CLOUD_PROJECT: ${GOOGLE_CLOUD_PROJECT}"
-GOOGLE_APPLICATION_CREDENTIALS="${HOME}/.config/gcloud/${GOOGLE_CLOUD_PROJECT}.json"
+GOOGLE_APPLICATION_CREDENTIALS=${HOME}/.config/gcloud/${GOOGLE_CLOUD_PROJECT}.json
 echo "GOOGLE_APPLICATION_CREDENTIALS: ${GOOGLE_APPLICATION_CREDENTIALS}"
 echo $GOOGLE_APPLICATION_CREDENTIALS_CONTENT > ${GOOGLE_APPLICATION_CREDENTIALS}
 gcloud auth activate-service-account --key-file ${GOOGLE_APPLICATION_CREDENTIALS}
-BILLING_ID="$(gcloud beta billing accounts list --format=json | jq '.[0].name[16:]')"
+BILLING_ID=$(gcloud beta billing accounts list --format=json | jq '.[0].name[16:]')
 echo "BILLING_ID: ${BILLING_ID}"
 
 terrahub --version > /dev/null 2>&1 || { echo >&2 'terrahub is missing. aborting...'; exit 1; }
