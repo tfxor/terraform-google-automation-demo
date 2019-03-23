fbb024a99c5a30d02416c16896ce18b9086ac55d
Eugene <eistrati@mitocgroup.com>
2019-03-23 13:52:49 -0400

    fixed bug with export statement

--- bin/deploy-codepipeline.sh
+++ bin/deploy-codepipeline.sh
@@ -15,12 +15,10 @@ git checkout $BRANCH_TO
 git checkout $BRANCH_FROM
 
 export GOOGLE_CLOUD_PROJECT="$(gcloud config list --format=json | jq -r '.core.project')"
-echo "GOOGLE_CLOUD_PROJECT: ${GOOGLE_CLOUD_PROJECT}"
 export GOOGLE_APPLICATION_CREDENTIALS="${HOME}/.config/gcloud/${GOOGLE_CLOUD_PROJECT}.json"
-echo "GOOGLE_APPLICATION_CREDENTIALS: ${GOOGLE_APPLICATION_CREDENTIALS}"
 echo $GOOGLE_APPLICATION_CREDENTIALS_CONTENT > ${GOOGLE_APPLICATION_CREDENTIALS}
 gcloud auth activate-service-account --key-file ${GOOGLE_APPLICATION_CREDENTIALS}
-export BILLING_ID="$(gcloud beta billing accounts list --format=json | jq -r '.[0].name[16:]')"
+BILLING_ID="$(gcloud beta billing accounts list --format=json | jq -r '.[0].name[16:]')"
 
 terrahub --version > /dev/null 2>&1 || { echo >&2 'terrahub is missing. aborting...'; exit 1; }
 terrahub configure -c template.locals.google_project_id="${GOOGLE_CLOUD_PROJECT}"
