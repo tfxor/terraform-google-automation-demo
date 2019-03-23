4ca0c9b71123bfe8ef0df2fc08b1f93f9593c306
Eugene <eistrati@mitocgroup.com>
2019-03-23 13:15:32 -0400

    minor improvements

--- bin/deploy-codepipeline.sh
+++ bin/deploy-codepipeline.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 gcloud --version > /dev/null 2>&1 || { echo >&2 'gcloud is missing. aborting...'; exit 1; }
-npm --version > /dev/null 2>&1 || { echo >&2 "npm is missing. aborting..."; exit 1; }
+jq --version > /dev/null 2>&1 || { echo >&2 'jq is missing. aborting...'; exit 1; }
+npm --version > /dev/null 2>&1 || { echo >&2 'npm is missing. aborting...'; exit 1; }
 export NODE_PATH="$(npm root -g)"
 
 if [ -z "${BRANCH_FROM}" ]; then BRANCH_FROM = "dev"; fi
@@ -9,18 +10,18 @@ if [ -z "${BRANCH_TO}" ]; then BRANCH_TO = "dev"; fi
 if [ "${BRANCH_TO}" != "dev" ]; then THUB_ENV="-e ${BRANCH_TO}"; fi
 if [ "${THUB_STATE}" == "approved" ]; then THUB_APPLY="-a"; fi
 
-git --version > /dev/null 2>&1 || { echo >&2 "git is missing. aborting..."; exit 1; }
+git --version > /dev/null 2>&1 || { echo >&2 'git is missing. aborting...'; exit 1; }
 git checkout $BRANCH_TO
 git checkout $BRANCH_FROM
 
-jq --version > /dev/null 2>&1 || { echo >&2 'jq is missing. aborting...'; exit 1; }
 GOOGLE_CLOUD_PROJECT="$(gcloud config list --format=json | jq '.core.project')"
-cat $GOOGLE_APPLICATION_CREDENTIALS_CONTENT > ${HOME}/${GOOGLE_CLOUD_PROJECT}.json
-BILLING_ID="$(gcloud beta billing accounts list --format=json | jq '.[0].name[16:]')"
 GOOGLE_APPLICATION_CREDENTIALS="${HOME}/${GOOGLE_CLOUD_PROJECT}.json"
+echo $GOOGLE_APPLICATION_CREDENTIALS_CONTENT > ${GOOGLE_APPLICATION_CREDENTIALS}
+gcloud auth activate-service-account --key-file ${GOOGLE_APPLICATION_CREDENTIALS}
+BILLING_ID="$(gcloud beta billing accounts list --format=json | jq '.[0].name[16:]')"
 SERVICE_ACCOUNT="terraform@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com"
 
-terrahub --version > /dev/null 2>&1 || { echo >&2 "terrahub is missing. aborting..."; exit 1; }
+terrahub --version > /dev/null 2>&1 || { echo >&2 'terrahub is missing. aborting...'; exit 1; }
 terrahub configure -c template.locals.google_project_id="${GOOGLE_CLOUD_PROJECT}"
 terrahub configure -c template.locals.google_billing_account="${BILLING_ID}"
 
