91cd5fde56c81776870d059e9ce8cc5bfda2ff17
root <root@DESKTOP-ECSQPB1.localdomain>
2019-03-25 18:40:14 +0100

    removed mapping

--- bin/deploy-codepipeline.sh
+++ bin/deploy-codepipeline.sh
@@ -17,7 +17,7 @@ git clone https://github.com/TerraHubCorp/www.git && rm -rf ./www/.terrahub*
 export GOOGLE_CLOUD_PROJECT="$(gcloud config list --format=json | jq -r '.core.project')"
 if [ "${GOOGLE_CLOUD_PROJECT}" == "null" ]; then echo >&2 'gcloud core project is missing. aborting...'; exit 1; fi
 export GOOGLE_APPLICATION_CREDENTIALS="${HOME}/.config/gcloud/${GOOGLE_CLOUD_PROJECT}.json"
-# echo ${GOOGLE_APPLICATION_CREDENTIALS_CONTENT} > ${GOOGLE_APPLICATION_CREDENTIALS}
+echo ${GOOGLE_APPLICATION_CREDENTIALS_CONTENT} > ${GOOGLE_APPLICATION_CREDENTIALS}
 gcloud auth activate-service-account --key-file ${GOOGLE_APPLICATION_CREDENTIALS}
 BILLING_ID="$(gcloud beta billing accounts list --format=json | jq -r '.[0].name[16:]')"
 
